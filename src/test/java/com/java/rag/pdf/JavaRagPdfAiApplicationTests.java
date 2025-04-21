70fb91d73a6abb4f5c0652d76f43eecd45eb77f1
Shubrato Debnath <shubratodn44985@gmail.com>
2025-04-21 11:41:54 +0600

    Inserting the base project

---
+++ src/test/java/com/java/rag/pdf/JavaRagPdfAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.rag.pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRagPdfAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
